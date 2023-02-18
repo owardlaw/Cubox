ad661b4894b3e43acc65c9e921bcb3a4efda7331
owen <owen@dermose.com>
2023-02-18 13:35:05 -0800

    adding DB classes for all cube states

--- src/Main.js
+++ src/Main.js
@@ -19,55 +19,60 @@ function Main() {
   React.useEffect(() => {
     generateScramble(cube, setCcramble)
   }, []);
-  
-    useEffect(() => {
-      // Open the database and get all times
-      async function getTimes() {
-        const db = await openDB('time-tracker', 1, {
-          upgrade(db) {
-            db.createObjectStore('times', { keyPath: 'id', autoIncrement: true });
-          },
-        });
-        const times = await db.getAll('times');
-  
-        // convert all db times to ints or set to null if not possible
-        const intTimes = times.map((time) => {
-          const parsedTime = parseInt(time.time, 10);
-          return isNaN(parsedTime) ? null : parsedTime;
-        });
-  
-        setTimes(intTimes);
-      }
-  
-      getTimes();
-    }, []);
-  
-     async function addTime(time) {
-      // Add a new time to the database with the array index as the key
-      const db = await openDB('time-tracker', 1);
-      const tx = db.transaction('times', 'readwrite');
-      const store = tx.objectStore('times');
-      const index = times.length; // get the current length of the array as the key
-      await store.add({ id: index, time }); // include the index as the key
-      // setTimes([...times, { id: index, time }]); // include the new item in the state
-    }
-  
-    // Delete all times 
-    async function deleteTimes() {
-      const db = await openDB('time-tracker', 1);
-      const tx = db.transaction('times', 'readwrite');
-      const store = tx.objectStore('times');
-      await store.clear();
-      setTimes([]);
-    };
-  
+
+  // Define the database and object store names based on the cube state
+  const dbName = cube + "times";
+  const dbObjectStore = cube;
+
+  // Get the times associated with the current cube state
+  async function getTimes() {
+    const db = await openDB(dbName, 1, {
+      upgrade(db) {
+        db.createObjectStore(dbObjectStore, { keyPath: 'id', autoIncrement: true });
+
+      },
+    });
+    const times = await db.getAll(dbObjectStore);
+
+    // convert all db times to ints or set to null if not possible
+    const intTimes = times.map((time) => {
+      const parsedTime = parseInt(time.time, 10);
+      return isNaN(parsedTime) ? null : parsedTime;
+    });
+
+    setTimes(intTimes);
+  }
+
+  // Retrieve the initial times on startup and whenever the cube state changes
+  useEffect(() => {
+    getTimes();
+  }, [cube]);
+
+  async function addTime(time) {
+    // Add a new time to the database with the array index as the key
+    const db = await openDB(dbName, 1);
+    const tx = db.transaction(dbObjectStore, 'readwrite');
+    const store = tx.objectStore(dbObjectStore);
+    const index = times.length; // get the current length of the array as the key
+    await store.add({ id: index, time }); // include the index as the key
+  }
+
+  // Delete all times 
+  async function deleteTimes() {
+    const db = await openDB(dbName, 1);
+    const tx = db.transaction(dbObjectStore, 'readwrite');
+    const store = tx.objectStore(dbObjectStore);
+    await store.clear();
+    setTimes([]);
+  };
+
 
   return (
     <div className="top-container">
 
       <div className='header'>
         <p id="scramble">{scramble}</p>
-        <CubeButtons setCube={setCube} generateScramble={generateScramble} setCcramble={setCcramble}/>
+        <CubeButtons setCube={setCube} generateScramble={generateScramble} setCcramble={setCcramble} />
         <div>
           <button id="scramble-buttons" onClick={() => generateScramble(cube, setCcramble)}>Next Scramble</button>
         </div>
@@ -75,11 +80,11 @@ function Main() {
 
       <div className="grid-container">
         <div className="left-grid">
-          <Table times={times} deleteTimes={deleteTimes}/>
+          <Table times={times} deleteTimes={deleteTimes} />
         </div>
 
         <div className="middle-grid">
-          <Timer cube={cube} times={times} setTimes={setTimes} setCcramble={setCcramble} generateScramble={generateScramble}  addTime={addTime}/>
+          <Timer cube={cube} times={times} setTimes={setTimes} setCcramble={setCcramble} generateScramble={generateScramble} addTime={addTime} />
         </div>
 
         <div className="right-grid">
@@ -93,7 +98,7 @@ function Main() {
             <LineChart data={times} />
           </div>
         </div>
-        
+
       </div>
     </div>
   );
